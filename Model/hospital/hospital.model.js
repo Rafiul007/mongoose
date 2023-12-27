0ee4be6d899bc62688ad220f366dcedfe9d16188
Rafie <rafiul.faisal@gmail.com>
2023-12-27 17:53:51 +0600

    mongoose model

---
+++ Model/hospital/hospital.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    specializedIn:[
+        {
+            type: String
+        }
+    ]
+}, { timestamps: true })
+export const Hospital = mongoose.model('Hospital', hospitalSchema);
